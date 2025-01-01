50fb314d0f8d0d25444c529399afa435ee8725f1
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-01 18:44:51 +0330

    style css and js code

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 let btn=document.querySelector('body');
+btn.addEventListener('click',function(e){
+let x=e.clientX;
+let y=e.clientY;
+
+let tag=document.createElement('span');
+document.body.appendChild(tag);
+tag.style.left=x+'px';
+tag.style.top=y+'px';
+
+
+})
